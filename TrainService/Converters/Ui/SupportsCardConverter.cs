6819313bdf973f59abee7773176af44453398ef5
xiaojiejiewoxiang <865948743@qq.com>
2017-08-26 23:00:16 +0800

    上传项目内容

---
+++ TrainService/Converters/Ui/SupportsCardConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TrainService.Converters.Ui {
+    public class SupportsCardConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if(value == null)
+                return null;
+            var SupportsCard = (string)value;
+            return SupportsCard == "0" ? "不支持" : "支持";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            if(value == null)
+                return null;
+            var SupportsCardString = (string)value;
+            return SupportsCardString == "不支持" ? 0 : 1;
+        }
+    }
+}
---
+++ TrainService/Converters/Ui/TrainSeatFeatureConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TrainService.Converters.Ui
+{
+    public class TrainSeatFeatureConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            var trainSeatFeature = (int) value;
+            return trainSeatFeature == 0 ? "无" : "有";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            var trainSeatFeatureString = (string) value;
+            return trainSeatFeatureString == "无" ? 0 : 3;
+        }
+    }
+}
