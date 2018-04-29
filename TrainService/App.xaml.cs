bd936da618c2dfebf30c085264963d997a0ef77b
zhanwh9 <641240673@qq.com>
2018-04-29 21:09:08 +0800

    提交新的UI文件

---
+++ TrainService/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Windows;
+using TrainService.LocalData;
+using TrainService.Policies;
+
+namespace TrainService
+{
+    /// <summary>
+    ///     App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Runtime.Load();
+            ServicePointManager.ServerCertificateValidationCallback = CertificateValidation.ValidateServerCertificate;
+        }
+    }
+}
